efb64c777f1ff0510af9db0a7a0dc2b78cb4ea6b
Erik <erik.antonyan1994@gmial.com>
2020-09-26 23:34:12 +0400

    Implement Statistics screen

--- app/src/main/java/com/example/runningtracker/ui/statistics/StatisticsViewModel.kt
+++ app/src/main/java/com/example/runningtracker/ui/statistics/StatisticsViewModel.kt
@@ -5,4 +5,11 @@ import androidx.lifecycle.ViewModel
 import com.example.runningtracker.datarepository.DataRepository
 
 class StatisticsViewModel @ViewModelInject constructor(private val dataRepository: DataRepository): ViewModel() {
+
+    val totalTimeRun = dataRepository.getTotalDurationInMillis()
+    val totalDistance = dataRepository.getTotalDistance()
+    val totalCaloriesBurned = dataRepository.getTotalBurnedCalories()
+    val totalAvgSpeed = dataRepository.getTotalAverageSpeed()
+
+    val runsSortedByDate = dataRepository.getAllRunsSortedByDates()
 }
